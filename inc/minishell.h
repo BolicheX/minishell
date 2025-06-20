f144f5abfbe3b47e8b5f16a9b18181b9e91c85f8
jose_portatil <joseantonlan@hotmail.com>
2025-06-20 17:23:28 +0200

    added makefile and header

---
+++ inc/minishell.h
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jose-jim <jose-jim@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/20 16:35:33 by jose-jim          #+#    #+#             */
+/*   Updated: 2025/06/20 17:22:09 by jose-jim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <signal.h>
+# include <dirent.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+
+
+typedef enum e_token_type
+{
+	T_WORD,
+	T_PIPE,
+	T_REDIR,
+	T_QUOTE
+}	t_token_type;
+
+typedef struct s_token
+{
+	char			*value;
+	t_token_type	type;
+	struct s_token	*next;
+}	t_token;
+
+typedef struct s_redir
+{
+	int				type;
+	char			*file;
+	struct s_redir	*next;
+}	t_redir;
+
+typedef struct s_env
+{
+	char			*key;
+	char			*value;
+	struct s_env	*next;
+}	t_env;
+
+typedef struct s_ms
+{
+	t_cmd	*cmd_list;
+	t_env	*env_list;
+	int		status;
+	pid_t	pid;
+}		t_ms;
+
+typedef struct s_cmd
+{
+	char	**argv;
+	char	*path;
+	int		in;
+	int		out;
+	t_cmd	*next;
+}		t_cmd;
